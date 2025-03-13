96953a53ab372546b7779dd08798b7d63e6d23a4
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2025-03-12 21:53:34 -0300

    add structure for Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,39 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+        <section className="footer__section-icons-imgs">
+          <section className="footer__payment-method">
+            <p>Medios de pago:</p>
+            <div className="footer__payment-method__visa">
+              <img src="imgs/visa.webp" alt="Tarjeta Visa" />
+            </div>
+            <div className="footer__payment-method__mastercard">
+              <img src="imgs/mastercard.webp" alt="Tarjeta Mastercard" />
+            </div>
+            <div className="footer__payment-method__mercado-pago">
+              <img src="imgs/mercado-pago.webp" alt="Mercado Pago" />
+            </div>
+          </section>
+
+          <section className="footer__icons">
+            <p>Buscanos en nuestras redes sociales:</p>
+            <div className="footer__icon-instagram">
+              <i className="fa-brands fa-instagram"></i>
+            </div>
+            <div className="footer__icon-facebook">
+              <i className="fa-brands fa-square-facebook"></i>
+            </div>
+            <div className="footer__icon-correo">
+              <i className="fa-solid fa-envelope"></i>
+            </div>
+          </section>
+        </section>
+
+        <section className="footer__copyrigth">
+          ® COPYRIGTH SOFIA DE ALESSANDRE 2025
+        </section>
+      </footer>
   )
 }
 
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const Inicio = () => {
@@ -14,40 +15,8 @@ const Inicio = () => {
 
         <section className="cards-container" id="container-products"></section>
       </main>
-
-      <footer className="footer">
-        <section className="footer__section-icons-imgs">
-          <section className="footer__payment-method">
-            <p>Medios de pago:</p>
-            <div className="footer__payment-method__visa">
-              <img src="imgs/visa.webp" alt="Tarjeta Visa" />
-            </div>
-            <div className="footer__payment-method__mastercard">
-              <img src="imgs/mastercard.webp" alt="Tarjeta Mastercard" />
-            </div>
-            <div className="footer__payment-method__mercado-pago">
-              <img src="imgs/mercado-pago.webp" alt="Mercado Pago" />
-            </div>
-          </section>
-
-          <section className="footer__icons">
-            <p>Buscanos en nuestras redes sociales:</p>
-            <div className="footer__icon-instagram">
-              <i className="fa-brands fa-instagram"></i>
-            </div>
-            <div className="footer__icon-facebook">
-              <i className="fa-brands fa-square-facebook"></i>
-            </div>
-            <div className="footer__icon-correo">
-              <i className="fa-solid fa-envelope"></i>
-            </div>
-          </section>
-        </section>
-
-        <section className="footer__copyrigth">
-          ® COPYRIGTH SOFIA DE ALESSANDRE 2025
-        </section>
-      </footer>
+<Footer/>
+      
     </>
   );
 };
