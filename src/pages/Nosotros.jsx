dc4bf8fd66b8b6e58a633352f5a7783dbd5795c8
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2025-04-01 20:21:36 -0300

    replace 'class' by 'classname'

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -4,14 +4,14 @@ import "./Nosotros.scss";
 const Nosotros = () => {
   useTitulo("Nosotros");
   return (
-    <main class="main__acerca">
-      <section class="section__acerca">
-        <header class="section__acerca__header">
+    <main className="main__acerca">
+      <section className="section__acerca">
+        <header className="section__acerca__header">
           <h1>Conocenos</h1>
           <p>¿Quiénes somos?</p>
         </header>
       </section>
-      <section class="section__acerca__descripcion">
+      <section className="section__acerca__descripcion">
         Somos amantes de la teconología, es por eso que queremos garantizarte lo
         mejor en electro
       </section>
